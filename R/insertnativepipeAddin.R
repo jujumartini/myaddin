1bf70074ad3c67da6fff3ff7f96583db0bcd73d1
Julian END416 <marti994@uwm.edu>
2022-04-19 17:46:07 -0500

    feature(R/man): insert native pipe Addin

--- R/insertnativepipeAddin.R
+++ R/insertnativepipeAddin.R
@@ -1,3 +1,16 @@
+#' @title Insert |> operator
+#'
+#' @description Call this function as an addin to insert \code{ |> } at the cursor position.
+#'
+#' @return |>
+#'
+#' @aliases nativepipe
+#'
+#' @importFrom  rstudioapi insertText
+#'
+#' @export
+#'
+
 insertnativepipeAddin <- function() {
-  rstudioapi::insertText(" |> ")
+  insertText(" |> ")
 }
